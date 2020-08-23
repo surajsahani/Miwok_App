5743170668790864ef7e7d4fb1587847c70d9569
Suraj sahani <surajkumarsahani1997@gmail.com>
2020-08-23 12:17:14 +0530

    Updated changes

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -33,18 +33,18 @@ public class PhrasesActivity extends AppCompatActivity {
 
         // Create a list of words
         final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("Where are you going?", "minto wuksus",
+        words.add(new Word("Where are you going?", "Doko ni iku no?\n",
                 R.raw.phrase_where_are_you_going));
-        words.add(new Word("What is your name?", "tinnә oyaase'nә",
+        words.add(new Word("What is your name?", "Onamaehanandesuka?",
                 R.raw.phrase_what_is_your_name));
-        words.add(new Word("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
-        words.add(new Word("How are you feeling?", "michәksәs?", R.raw.phrase_how_are_you_feeling));
-        words.add(new Word("I’m feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
-        words.add(new Word("Are you coming?", "әәnәs'aa?", R.raw.phrase_are_you_coming));
-        words.add(new Word("Yes, I’m coming.", "hәә’ әәnәm", R.raw.phrase_yes_im_coming));
-        words.add(new Word("I’m coming.", "әәnәm", R.raw.phrase_im_coming));
-        words.add(new Word("Let’s go.", "yoowutis", R.raw.phrase_lets_go));
-        words.add(new Word("Come here.", "әnni'nem", R.raw.phrase_come_here));
+        words.add(new Word("My name is...", "Watashinonamaeha...", R.raw.phrase_my_name_is));
+        words.add(new Word("How are you feeling?", "Go kibun wa ikagadesu ka?", R.raw.phrase_how_are_you_feeling));
+        words.add(new Word("I’m feeling good.", "Watashi wa kibungayoidesu.", R.raw.phrase_im_feeling_good));
+        words.add(new Word("Are you coming?", "Kimasu ka?", R.raw.phrase_are_you_coming));
+        words.add(new Word("Yes, I’m coming.", "Hai, kimasu\n", R.raw.phrase_yes_im_coming));
+        words.add(new Word("I’m coming.", "Ima okonatteru", R.raw.phrase_im_coming));
+        words.add(new Word("Let’s go.", "Yukō", R.raw.phrase_lets_go));
+        words.add(new Word("Come here.", "Koko ni kite", R.raw.phrase_come_here));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
